b76fc982d0153e3b18578102697eaca2e7b73598
Seyrit <seyrit16@mail.ru>
2025-04-13 21:00:57 +0300

    create AuthService

---
+++ user_service/src/main/java/com/example/user_service/service/security/AuthenticationService.java
@@ -0,0 +1,12 @@
+package com.example.user_service.service.security;
+
+import com.example.user_service.dto.auth.request.SignUpRequest;
+import com.example.user_service.model.User;
+
+public interface AuthenticationService {
+    User signUpUser(SignUpRequest dto);
+    Boolean checkIsValidCodeByEmail(String email, String verifyCode);
+    String signInWithCode(String email, String verifyCode);
+    String signInWithPassword(String email, String password);
+    void sendVerificationCode(String email);
+}
