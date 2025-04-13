ce3fde25b0144b4db6437d1aebea8b44f5787318
Seyrit <seyrit16@mail.ru>
2025-04-13 20:54:04 +0300

    create functional for JWT

---
+++ user_service/src/main/java/com/example/user_service/config/security/components/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package com.example.user_service.config.security.components;
+
+import com.example.user_service.service.security.JwtService;
+import com.example.user_service.service.security.impl.CustomUserDetailsService;
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    public static final String BEARER_PREFIX = "Bearer ";
+    public static final String HEADER_NAME = "Authorization";
+    private final JwtService jwtService;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public JwtAuthenticationFilter(JwtService jwtService, CustomUserDetailsService customUserDetailsService) {
+        this.jwtService = jwtService;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException, java.io.IOException {
+        // Получаем токен из заголовка
+        String authHeader = request.getHeader(HEADER_NAME);
+        if (StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader, BEARER_PREFIX)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        //обрезаем префикс и получаем email из токена
+        String jwt = authHeader.substring(BEARER_PREFIX.length());
+        String email = jwtService.extractEmail(jwt);
+
+        if (StringUtils.isNotEmpty(email) && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
+
+            // Если токен валиден, то аутентифицируем пользователя
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                SecurityContext context = SecurityContextHolder.createEmptyContext();
+
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                context.setAuthentication(authenticationToken);
+                SecurityContextHolder.setContext(context);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ user_service/src/main/java/com/example/user_service/service/security/JwtService.java
@@ -0,0 +1,156 @@
+package com.example.user_service.service.security;
+
+
+import com.example.user_service.config.security.components.CustomUserDetails;
+import com.example.user_service.config.security.components.JwtData;
+import com.example.user_service.invariant.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cglib.core.internal.Function;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    @Value("${token.signing.key}")
+    private String jwtSigningKey;
+    private SecretKey signingKey;
+
+    @PostConstruct
+    public void init() {
+
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSigningKey);
+        this.signingKey = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractEmail(String token) {
+
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Long extractId(String token) {
+
+        return extractClaim(token, claims -> claims.get("id", Long.class));
+    }
+
+    public JwtData extractData(String token) {
+        return JwtData.builder()
+                .id(extractId(token))
+                .email(extractEmail(token))
+                .role(extractClaim(token, claims -> {
+                    String roleStr = claims.get("role", String.class);
+                    return roleStr != null ? Role.valueOf(roleStr) : null;
+                }))
+                .createdDateTime(extractClaim(token, claims -> {
+                    String cdts = claims.get("createdDateTime", String.class);
+                    return cdts != null ? LocalDateTime.parse(cdts) : null;
+                }))
+                .build();
+    }
+
+    public String generateToken(UserDetails userDetails) {
+
+        Map<String, Object> claims = new HashMap<>();
+        if (userDetails instanceof CustomUserDetails customUserDetails) {
+            claims.put("id", customUserDetails.getId());
+            claims.put("email", customUserDetails.getUsername());
+            claims.put("role", customUserDetails.getRole());
+            claims.put("createdDateTime", LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        }
+        return generateToken(claims, userDetails);
+    }
+
+    public String refreshToken(UserDetails userDetails) {
+        return generateToken(userDetails);
+    }
+
+    public Boolean isTokenValid(String token, UserDetails userDetails) {
+        final String userName = extractEmail(token);
+        return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    /**
+     * Извлечение данных из токена
+     *
+     * @param token           токен
+     * @param claimsResolvers функция извлечения данных
+     * @param <T>             тип данных
+     * @return данные
+     */
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
+
+        final Claims claims = extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
+
+    /**
+     * Генерация токена
+     *
+     * @param extraClaims дополнительные данные
+     * @param userDetails данные пользователя
+     * @return токен
+     */
+    private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+
+        return Jwts.builder().claims(extraClaims).subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 100000 * 60 * 24))
+                .signWith(getSigningKey(), Jwts.SIG.HS256).compact();
+    }
+
+    /**
+     * Проверка токена на просроченность
+     *
+     * @param token токен
+     * @return true, если токен просрочен
+     */
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    /**
+     * Извлечение даты истечения токена
+     *
+     * @param token токен
+     * @return дата истечения
+     */
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    /**
+     * Извлечение всех данных из токена
+     *
+     * @param token токен
+     * @return данные
+     */
+    private Claims extractAllClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    /**
+     * Получение ключа для подписи токена
+     *
+     * @return ключ
+     */
+    private SecretKey getSigningKey() {
+        return signingKey;
+    }
+}
