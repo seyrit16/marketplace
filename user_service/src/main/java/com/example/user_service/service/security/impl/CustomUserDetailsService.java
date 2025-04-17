d60ec6dba198323cdaa17d6b5f84bd332fb64089
Seyrit <seyrit16@mail.ru>
2025-04-17 22:35:43 +0300

    Update CustomUserDetailsService.java

--- user_service/src/main/java/com/example/user_service/service/security/impl/CustomUserDetailsService.java
+++ user_service/src/main/java/com/example/user_service/service/security/impl/CustomUserDetailsService.java
@@ -1,9 +1,9 @@
 package com.example.user_service.service.security.impl;
 
 import com.example.user_service.config.security.components.CustomUserDetails;
-import com.example.user_service.dto.UserDTO;
 import com.example.user_service.model.User;
 import com.example.user_service.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,27 +16,17 @@ import java.util.Optional;
 @Primary
 public class CustomUserDetailsService implements UserDetailsService{
 
+    @Autowired
     private UserService userService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userService.getUserByEmail(email);
-        return getUser(user).map(CustomUserDetails::new).orElseThrow(()->new UsernameNotFoundException("Такой адрес электронной почты не найден."));
-    }
 
-    private Optional<UserDTO> getUser(User user){
-        if(user == null) {
-            return Optional.empty();
+        if(user == null){
+           throw new UsernameNotFoundException("Такой адрес электронной почты не найден.");
         }
 
-        UserDTO userDTO =UserDTO.builder()
-                .id(user.getId())
-                .email(user.getEmail())
-                .password(user.getPassword())
-                .role(user.getRole())
-                .isActive(user.getIsActive())
-                .isLocked(user.getIsLocked())
-                .build();
-        return Optional.of(userDTO);
+        return new CustomUserDetails(user);
     }
 }
