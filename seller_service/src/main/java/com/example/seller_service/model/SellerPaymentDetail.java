4eb0fcbb472f8574110987e2c9c1d4c255762a54
Seyrit <seyrit16@mail.ru>
2025-04-18 17:15:15 +0300

    crete models

---
+++ seller_service/src/main/java/com/example/seller_service/model/SellerPaymentDetail.java
@@ -0,0 +1,41 @@
+package com.example.seller_service.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity
+@Table(name = "seller_payment_details")
+public class SellerPaymentDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "seller_id", nullable = false)
+    private SellerProfile seller;
+
+    @Column(name = "bank_account_number", nullable = false)
+    private String bankAccountNumber;
+
+    @Column(name = "bank_name", nullable = false)
+    private String bankName;
+
+    @Column(name = "bic", nullable = false)
+    private String bic;
+
+    @Column(name = "account_holder_name", nullable = false)
+    private String accountHolderName;
+
+    @Column(name = "inn")
+    private String inn;
+
+    @Column(name = "is_verified")
+    private Boolean isVerified = false;
+}
---
+++ seller_service/src/main/java/com/example/seller_service/model/SellerProfile.java
@@ -0,0 +1,42 @@
+package com.example.seller_service.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity
+@Table(name = "sellers")
+public class SellerProfile {
+    @Id
+    private UUID id;
+
+    @Column(name = "user_id",nullable = false)
+    private UUID userId;
+
+    @OneToOne(mappedBy = "seller", cascade = CascadeType.ALL)
+    @JoinColumn(name = "person_detail_id",nullable = false)
+    private PersonDetail personDetail;
+
+    @Column(name = "full_company_name",nullable = false)
+    private String fullCompanyName;
+
+    @Column(name = "short_company_name",nullable = false)
+    private String shortCompanyName;
+
+    @Column(name = "is_verified",nullable = false)
+    private Boolean isVerified=false;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToOne(mappedBy = "seller", cascade = CascadeType.ALL)
+    @JoinColumn(name = "seller_payment_detail_id")
+    private SellerPaymentDetail sellerPaymentDetail;
+}
