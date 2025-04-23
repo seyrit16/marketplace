bc002b4a6144326268cd98e1d44af2ceab1128cc
Seyrit <seyrit16@mail.ru>
2025-04-23 12:48:06 +0300

    update services

---
+++ seller_service/src/main/java/com/example/seller_service/service/mapper/PaymentDetailMapper.java
@@ -0,0 +1,24 @@
+package com.example.seller_service.service.mapper;
+
+import com.example.seller_service.dto.request.seller.PaymentDetailCreateRequest;
+import com.example.seller_service.dto.request.seller.put.PaymentDetailUpdateRequest;
+import com.example.seller_service.dto.response.seller.PaymentDetailUpdateResponse;
+import com.example.seller_service.model.PaymentDetail;
+import org.mapstruct.*;
+
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface PaymentDetailMapper {
+
+    PaymentDetail fromPaymentDetailCreateRequest(PaymentDetailCreateRequest paymentDetailCreateRequest);
+
+    PaymentDetailUpdateResponse toPaymentDetailUpdateResponse(PaymentDetail paymentDetail);
+
+    @AfterMapping
+    default void linkBack(@MappingTarget PaymentDetail detail, PaymentDetailCreateRequest request) {
+    }
+
+    @BeanMapping(nullValuePropertyMappingStrategy = IGNORE)
+    void updatePaymentDetailFromDTO(PaymentDetailUpdateRequest paymentDetailUpdateDTO, @MappingTarget PaymentDetail paymentDetail);
+}
