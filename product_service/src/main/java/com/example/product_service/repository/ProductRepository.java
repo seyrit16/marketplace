a210c477d2a2ad33d9f775128935aebea3359b85
Seyrit <seyrit16@mail.ru>
2025-04-25 19:05:17 +0300

    Update ProductRepository.java

--- product_service/src/main/java/com/example/product_service/repository/ProductRepository.java
+++ product_service/src/main/java/com/example/product_service/repository/ProductRepository.java
@@ -1,11 +1,15 @@
 package com.example.product_service.repository;
 
+import co.elastic.clients.elasticsearch.nodes.OperatingSystem;
 import com.example.product_service.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+    Optional<Product> findAllBySellerId(UUID id);
 }
