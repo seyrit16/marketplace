5849b11b8788fe4a976913902267942b628002dc
Seyrit <seyrit16@mail.ru>
2025-05-12 11:20:35 +0300

    (seller service) complete

--- seller_service/src/main/java/com/example/seller_service/dto/request/seller/PaymentDetailCreateRequest.java
+++ seller_service/src/main/java/com/example/seller_service/dto/request/seller/PaymentDetailCreateRequest.java
@@ -2,15 +2,26 @@ package com.example.seller_service.dto.request.seller;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class PaymentDetailCreateRequest {
+    @NotBlank(message = "Номер банковского счета не должен быть пустым")
+    @Pattern(regexp = "\\d{20}", message = "Номер банковского счета должен содержать ровно 20 цифр")
     private String bankAccountNumber;
+    @NotBlank(message = "Название банка не должно быть пустым")
     private String bankName;
+    @NotBlank(message = "БИК не должен быть пустым")
+    @Pattern(regexp = "\\d{9}", message = "БИК должен содержать ровно 9 цифр")
     private String bic;
+    @NotBlank(message = "Имя владельца счета не должно быть пустым")
     private String accountHolderName;
+    @NotBlank(message = "ИНН не должен быть пустым")
+    @Pattern(regexp = "\\d{10}|\\d{12}", message = "ИНН должен содержать 10 или 12 цифр")
     private String inn;
 }
--- seller_service/src/main/java/com/example/seller_service/dto/request/seller/PersonCreateRequest.java
+++ seller_service/src/main/java/com/example/seller_service/dto/request/seller/PersonCreateRequest.java
@@ -2,14 +2,24 @@ package com.example.seller_service.dto.request.seller;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class PersonCreateRequest {
+    @NotBlank(message = "Фамилия не должна быть пустой")
     private String surname;
+
+    @NotBlank(message = "Имя не должно быть пустым")
     private String name;
+
     private String patronimyc;
+
+    @NotBlank(message = "Номер телефона не должен быть пустым")
+    @Pattern(regexp = "\\+?[1-9]\\d{1,14}", message = "Номер телефона должен быть валидным")
     private String phoneNumber;
 }
